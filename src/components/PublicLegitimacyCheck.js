d1b5ebb1454bd1af2c9b72c02e3a9759336629f6
shaunm1767-ui <shaunm1767@gmail.com>
2026-01-02 14:37:17 +0200

    Add PublicLegitimacyCheck component

---
+++ src/components/PublicLegitimacyCheck.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PublicLegitimacyCheck() {
+  return (
+    <div style={{ border: "1px solid #ccc", padding: "10px", margin: "5px" }}>
+      <h3>Public Legitimacy Check</h3>
+      <p>Feature to verify public legitimacy of devices coming soon.</p>
+    </div>
+  );
+}
