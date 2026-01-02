196f32844d33a13ec640af225a8e6f9e044081a9
shaunm1767-ui <shaunm1767@gmail.com>
2026-01-02 14:34:19 +0200

    Integrate InvoiceUpload into Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import DeviceCard from "./DeviceCard";
+import InvoiceUpload from "./InvoiceUpload";
 
 export default function Dashboard() {
   return (
     <div>
       <h1>ProofPoint Dashboard</h1>
       <DeviceCard deviceName="iPhone 15" serial="12345XYZ" />
+      <InvoiceUpload />
     </div>
   );
 }
