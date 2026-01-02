8111012a69bda1850491169c9b28948dc5f83f25
shaunm1767-ui <shaunm1767@gmail.com>
2026-01-02 14:35:45 +0200

    Add StolenDeviceReport component

---
+++ src/components/StolenDeviceReport.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function StolenDeviceReport() {
+  return (
+    <div style={{ border: "1px solid #ccc", padding: "10px", margin: "5px" }}>
+      <h3>Stolen Device Report</h3>
+      <p>Feature to report stolen devices coming soon.</p>
+    </div>
+  );
+}
